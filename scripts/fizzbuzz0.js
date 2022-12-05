12127ee84ca064e5e26d7068e7fd2f62f3196095
lrios002 <42810971+lrios002@users.noreply.github.com>
2022-12-05 03:08:49 -0500

    Add files via upload

---
+++ scripts/fizzbuzz0.js
@@ -0,0 +1,19 @@
+function displayName(){
+	var firstName = document.getElementById("first-name").value;
+	var middleInitial   = document.getElementById("middle-initial").value;
+    var lastName  = document.getElementById("last-name").value;
+    // Display Welcome to User Input
+			
+			document.getElementById("greeting").innerHTML = `Welcome to 360 live, ${firstName} ${middleInitial}. ${lastName}!`;			
+			var displayMessage = "";
+			var i:
+			for (i = 1; i <= 125; i++){
+			}
+			displayMessage += `${i}. We are here to helpe you!` + "<br>";
+			}
+			document.getElementById("demoZero").innerHTML = displayMessage;
+		}
+		
+		
+		
+
