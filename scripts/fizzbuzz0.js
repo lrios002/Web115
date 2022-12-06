f90559eaf7d805b39926166244892388a6aa72db
lrios002 <42810971+lrios002@users.noreply.github.com>
2022-12-05 19:39:00 -0500

    Add files via upload

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,15 +1,14 @@
-function fizzBuzz0(){
+function fizzbuzz0(){
 	var firstName = document.getElementById("first-name").value;
 	var middleInitial   = document.getElementById("middle-initial").value;
     var lastName  = document.getElementById("last-name").value;
     // Display Welcome to User Input
 			
-			document.getElementById("greeting").innerHTML = `Welcome to 360 live, ${firstName} ${middleInitial}. ${lastName}!`;			
+			document.getElementById("greeting").innerHTML = `Welcome to 360 live, ${firstName} ${middleInitial} ${lastName}!`;			
 			var displayMessage = "";
-			var i:
+			var i;
 			for (i = 1; i <= 125; i++){
-			}
-			displayMessage += `${i}. We are here to helpe you!` + "<br>";
+				displayMessage += `${i}. We are here to help you!` + "<br>";
 			}
 			document.getElementById("demoZero").innerHTML = displayMessage;
 		}
