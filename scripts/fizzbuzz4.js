8c8ab9ede2d590f2e59b577f4f297580a08e6c55
lrios002 <42810971+lrios002@users.noreply.github.com>
2022-12-10 00:55:19 -0500

    Update fizzbuzz4.js

--- scripts/fizzbuzz4.js
+++ scripts/fizzbuzz4.js
@@ -1,70 +1,79 @@
-function fizzbuzz4() {
+			
+	function fizzbuzz4(){
 	var firstName = document.getElementById("first-name").value;
 	var middleInitial   = document.getElementById("middle-initial").value;
 	var lastName  = document.getElementById("last-name").value;
+	var fizzNumber = 3;
+	var buzzNumber = 5; 
+	var bangNumber = 7; 
+
 	document.getElementById("greeting").innerHTML =  `Welcome to Live 360!, ${firstName} ${middleInitial}. ${lastName}!`;
 		
-	// LOOP MESSAGE - EVEN or ODD Number printed - with parameters and function checkDivision()
-  var displayMessage = "";
+	// Loop message / function checkDivision()
+	var displayMessage = "";
+	var i;
+	var word1 = "Fizz";
+	var word2 = "Buzz";
+	var word3 = "BANG!";
+	var wordCombo = word1 + word2 + word3;
+	var totalNumber = parseInt(document.getElementById("totalNumberCount").value);
 
-  var word1 = document.getElementById("firstWord").value; 
-  var word2 = document.getElementById("secondWord").value;
-  var word3 = document.getElementById("thirdWord").value;
-  var wordCombo = word1 + word2 + word3;
-
-  var firstNumber = document.getElementById("firstNumber").value; 
-  var secondNumber = document.getElementById("secondNumber").value; 
-  var thirdNumber = document.getElementById("thirdNumber").value; 
-  
-  
-  var i;
-
-  var totalNumber = parseInt(document.getElementById("totalNumberCount").value);
-
-  for (i = 1; i <= totalNumber; i++) 
-  {
-    displayMessage += `${i}. `;
-    if (checkDivision(i, firstNumber)  && checkDivision(i, secondNumber) && checkDivision(i, thirdNumber)) 
-    { 
-      displayMessage += wordCombo;
-    } 
-    else if (checkDivision(i, firstNumber) && checkDivision(i, secondNumber))
-    {
-      displayMessage += word1 + word2;
-    } 
-    else if (checkDivision(i, firstNumber) && checkDivision(i, thirdNumber))
-    {
-      displayMessage += word1 + word3;
-    } 
-    else if (checkDivision(i, secondNumber) && checkDivision(i, thirdNumber))
-    {
-      displayMessage += word2 + word3;
-    } 
-    else if (checkDivision(i, firstNumber))
-    {
-      displayMessage += word1;
-    } 
-    else if (checkDivision(i, secondNumber))
-    {
-      displayMessage += word2;
-    } 
-    else if (checkDivision(i, thirdNumber))
-    {
-      displayMessage += word3;
-    } 
-    else 
-    {
-      displayMessage += "";
+	for (i = 1; i <= 125; i++) 
+	{
+    displayMessage += `${i}. `
+	if (!checkDivision(i, fizzNumber)  && !checkDivision(i, buzzNumber) && !checkDivision(i, bangNumber)){
+		displayMessage += "Bee";
+	} else {
+		if (checkDivision(i, fizzNumber))
+		{
+		  displayMessage += word1 + " ";
+		} 
+		if (checkDivision(i, buzzNumber))
+		{
+		  displayMessage += word2+ " ";
+		} 
+		if (checkDivision(i, bangNumber))
+		{
+		  displayMessage += word3+ " ";
+		} 
     }
     displayMessage += "<br>";
 	}
 	document.getElementById("demoFour").innerHTML = displayMessage;
 }
 
-function checkDivision(num1, num2) { 
-  if(num1 % num2 === 0) {
+	function checkDivision(num1, num2) { 
+	if(num1 % num2 === 0) 
+	{
     return true;
-  } else {
+	} 
+	else 
+	{
     return false;
-  }
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
